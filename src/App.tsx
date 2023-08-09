6bea51464f1849b72da96f6ce617807b18fc696d
Andy <Andy.vasilev@gmail.com>
2023-08-09 11:54:45 +0300

    Add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from "react";
-import {Counter} from "./components/Counter";
-
+// libs
+import React, { Suspense } from "react";
+import {Link, Route, Routes} from "react-router-dom";
+// pages
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+// styles
 import "./index.scss";
 
 const App = () => {
     return (
         <div className="app">
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/About'}>О сайте</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
